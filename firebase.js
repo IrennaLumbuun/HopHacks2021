520807fd955733a9c32b4a939473fc43bd750800
Jiarui Ruan <jiaruiruan@plank-186.dynamic2.rpi.edu>
2021-09-19 01:46:11 -0400

    delete firebase key

--- firebase.js
+++ firebase.js
@@ -1,14 +1,5 @@
 import firebase from 'firebase/app';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD6GHBcdp8e-ukMy3Czxz8P65b6Eetf4Kg",
-  authDomain: "waste-fighters.firebaseapp.com",
-  projectId: "waste-fighters",
-  storageBucket: "waste-fighters.appspot.com",
-  messagingSenderId: "716160403468",
-  appId: "1:716160403468:web:d88542552cc3a413d58629"
-};
-
 if (firebase.apps.length === 0) {
   console.log('firebase', firebase.apps);
   firebase.initializeApp(firebaseConfig);
